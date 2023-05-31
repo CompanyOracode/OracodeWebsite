d2df1d974daf15b649d005839de8faf73e0439e2
CompanyOracode <Company.Oracode@gmail.com>
2023-05-31 21:04:04 +0200

    Text verändeung

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -2,13 +2,13 @@ export default function Logos() {
   return (
     <div className="Logos">
       <div className="JSLogo">
-        <h1 className="JSLogoText">Js</h1>
+        <h1 className="JSLogoText">JS</h1>
       </div>
       <div className="CSSLogo">
         <h1 className="CSSLogoText">CSS</h1>
       </div>
       <div className="HtmlLogo">
-        <h1 className="HtmlLogoText">Html</h1>
+        <h1 className="HtmlLogoText">HTML</h1>
       </div>
       <div className="PyLogo">
         <h1 className="PyLogoText">Py</h1>
